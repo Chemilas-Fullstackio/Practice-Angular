864ec54fa765a6b1c3b1743d48bdd9f2af168d0d
Chemilas <chemilas@fullstackio.com>
2026-04-10 17:13:35 +0530

    feature: Services section added

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../../shared/components/hero/hero";
 import { Stats } from '../stats/stats';
+import { Services } from "../services/services";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, Stats],
+  imports: [HeroComponent, Stats, Services],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- src/app/features/services/services.ts
+++ src/app/features/services/services.ts
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './services.scss',
 })
 export class Services {
+  title = 'Our services';
 
+  subtitle: string = "We bridge the gap between tradition and technology, helping farmers <br>achieve their goal with ease";
+
+  services: Service[] = [
+    {
+      imageUrl: "https://media.licdn.com/dms/image/sync/v2/D5627AQEtn03X_ELHZg/articleshare-shrink_800/B56ZyV9VZfKUAI-/0/1772042410681?e=2147483647&v=beta&t=CdP6cei5YDfYIfNT3m36UY7xZEZNKebA8AlQAmGbZcg",
+      title: "Farm Consultancy",
+      subtitle: "We provide advice on crop selection, irrigation and pest control."
+    },
+    {
+      imageUrl: "https://about.oceanstatejoblot.com/wp-content/uploads/2024/02/SF_HowDoGreenhousesWork_Introduction.webp",
+      title: "Sustainable Practices",
+      subtitle: "We specialize in integrating sustainable farming practices."
+    },
+    {
+      imageUrl: "https://www.shutterstock.com/image-photo/drone-shot-tractor-spraying-lush-600nw-2464594961.jpg",
+      title: "Equipment Rental",
+      subtitle: "Access to top-notch agricultural equipment for efficient farming."
+    }
+  ]
+
+}
+
+interface Service {
+  imageUrl: string;
+  title: string;
+  subtitle: string;
 }
